9bcdd66d78167c35567881da907ea52428d44e72
Anton Björkman <antonidag@hotmail.com>
2024-11-07 17:12:53 +0100

    add react flow with simple shapes

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 import {
   ReactFlow,
   addEdge,
@@ -9,12 +9,11 @@ import {
   Controls,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import React from 'react';
-import AceEditor from "react-ace";
+import AceEditor from 'react-ace';
 
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-monokai";
-import "ace-builds/src-noconflict/ext-language_tools";
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/ext-language_tools';
 
 
 const initialNodes = [
@@ -31,106 +30,49 @@ const initialNodes = [
     position: { x: 100, y: 100 },
     className: 'light',
     style: { backgroundColor: 'rgba(255, 0, 0, 0.2)', width: 200, height: 200 },
-  },
-  {
-    id: '2a',
-    data: { label: 'Node A.1' },
-    position: { x: 10, y: 50 },
-    parentId: '2',
-  },
-  {
-    id: '3',
-    data: { label: 'Node 1' },
-    position: { x: 320, y: 100 },
-    className: 'light',
-  },
-  {
-    id: '4',
-    data: { label: 'Group B' },
-    position: { x: 320, y: 200 },
-    className: 'light',
-    style: { backgroundColor: 'rgba(255, 0, 0, 0.2)', width: 300, height: 300 },
-    type: 'group',
-  },
-  {
-    id: '4a',
-    data: { label: 'Node B.1' },
-    position: { x: 15, y: 65 },
-    className: 'light',
-    parentId: '4',
-    extent: 'parent',
-  },
-  {
-    id: '4b',
-    data: { label: 'Group B.A' },
-    position: { x: 15, y: 120 },
-    className: 'light',
-    style: {
-      backgroundColor: 'rgba(255, 0, 255, 0.2)',
-      height: 150,
-      width: 270,
-    },
-    parentId: '4',
-  },
-  {
-    id: '4b1',
-    data: { label: 'Node B.A.1' },
-    position: { x: 20, y: 40 },
-    className: 'light',
-    parentId: '4b',
-  },
-  {
-    id: '4b2',
-    data: { label: 'Node B.A.2' },
-    position: { x: 100, y: 100 },
-    className: 'light',
-    parentId: '4b',
-  },
+  }
 ];
-
+ 
 const initialEdges = [
   { id: 'e1-2', source: '1', target: '2', animated: true },
-  { id: 'e1-3', source: '1', target: '3' },
-  { id: 'e2a-4a', source: '2a', target: '4a' },
-  { id: 'e3-4b', source: '3', target: '4b' },
-  { id: 'e4a-4b1', source: '4a', target: '4b1' },
-  { id: 'e4a-4b2', source: '4a', target: '4b2' },
-  { id: 'e4b1-4b2', source: '4b1', target: '4b2' },
 ];
 
-const NestedFlow = () => {
+const App: React.FC = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-  const onConnect = useCallback((connection) => {
-    setEdges((eds) => addEdge(connection, eds));
-  }, []);
+  
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
   return (
-    <ReactFlow
-    nodes={nodes}
-    edges={edges}
-    onNodesChange={onNodesChange}
-    onEdgesChange={onEdgesChange}
-    onConnect={onConnect}
-    className="react-flow-subflows-example"
-    fitView 
-  >
-    <MiniMap />
-    <Controls />
-    <Background />
-  </ReactFlow>
-    
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ width: '50%' }}>
+        <AceEditor
+          mode="javascript"
+          theme="monokai"
+          name="texteditor"
+          editorProps={{ $blockScrolling: false }}
+          height="100%"
+          width="100%"
+          showPrintMargin={false}
+        />
+      </div>
+      
+      <div style={{ width: '50%', position: 'relative' }}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          style={{ width: '100%', height: '100%' }}
+        >
+          <MiniMap />
+          <Controls />
+          <Background />
+        </ReactFlow>
+      </div>
+    </div>
   );
 };
 
-export default () => (
-<AceEditor
-    mode="javascript"
-    theme="monokai"
-    name="texteditor"
-    editorProps={{ $blockScrolling: false }}
-    height='100%'
-    width='50%'
-    showPrintMargin={false} />
-);
+export default App;
