8e24b5f34133641b33c817b52f756ec3c51c816f
Anton Björkman <antonidag@hotmail.com>
2025-02-07 10:09:25 +0100

    set up build out dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/flowitec/', // Replace <REPO_NAME> with your GitHub repository name
+  build: {
+    outDir: '_site',
+    emptyOutDir: true, // Ensures old builds are removed
+  }
 });
