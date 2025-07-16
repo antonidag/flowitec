22e236551e40e899928ab40bf5911ba706cd496f
Anton Björkman <antonidag@hotmail.com>
2025-07-16 22:10:23 +0200

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/flowitec/', // Replace <REPO_NAME> with your GitHub repository name
+  base: '/', // Set base to root for custom domain deployment
   // build: {
   //   outDir: '_site',
   //   emptyOutDir: true, // Ensures old builds are removed
